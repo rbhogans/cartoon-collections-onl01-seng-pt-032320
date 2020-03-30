bdfa01cb6dcb66ed4cb4189982a3daaeee51a5c2
Rabiah Hogans <hogansrabiah@gmail.com>
2020-03-30 23:35:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,5 @@
 def roll_call_dwarves(names)
-  names.each_with_index # code an argument here
-  # Your code here
+  names.each_with_index.collect {|name,index| puts"#{index+1}. #{name}"}
 end
 
 def summon_captain_planet# code an argument here
