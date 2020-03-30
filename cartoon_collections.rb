a072e7dde674298b2035421373dec25f4216249a
Rabiah Hogans <hogansrabiah@gmail.com>
2020-03-30 23:59:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,9 +6,9 @@ def summon_captain_planet(planeteer)
 planeteer.collect {|calls| calls.capitalize + "!"}
 end
 
-short_words = ["puff", "go", "two"]
-def long_planeteer_calls(short_words)
-
+def long_planeteer_calls(calls)
+calls.any? {|call| call.length>4}
+end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
